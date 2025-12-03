fc790629c885e1825b2756511d57b43a259afc82
ziad <ziadboukhalkhalofficiel@gmail.com>
2025-12-03 21:41:08 +0100

    downgrade spring version + screenshots

--- Cas 1/microservice-commandes/src/main/java/com/example/microservicecommandes/health/CommandesHealthIndicator.java
+++ Cas 1/microservice-commandes/src/main/java/com/example/microservicecommandes/health/CommandesHealthIndicator.java
@@ -2,8 +2,8 @@ package com.example.microservicecommandes.health;
 
 import com.example.microservicecommandes.service.CommandeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.health.contributor.Health;
-import org.springframework.boot.health.contributor.HealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
 @Component
